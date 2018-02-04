8e466288ddd5dab6a696f8f5fb8c9294a23fb58b
M. De Luca <ucabmpd@ucl.ac.uk>
2018-02-04 05:13:40 +0000

    second commit

--- samplingexample.py
+++ samplingexample.py
@@ -1,19 +1,20 @@
+import functionssampling as fs
 # This package provides objects that get samples of different distributions
 
 # For Normal distribution: normalSample(n,mu,sigma)
 # n: number of samples  (mu, sigma): parameters of Normal distribution
 # example: 
-x = normalSample(1000,5,0.1)
+x = fs.normalSample(1000,5,0.1)
 
 # For Poisson distribution: poissonSample(n, lambda)
 # n: number of samples  lambda: parameter of Poisson distribution
 # example: 
-y = poissonSample(1000,4)
+y = fs.poissonSample(1000,4)
 
 # For Binomial distribution: binomialSample(n, N, P)
 # n: number of samples  (N, P): parameters of Binomial distribution
 # example: 
-z = binomialSample(1000,100,0.1)
+z = fs.binomialSample(1000,100,0.1)
 
 # To get the summary of the different objects:
 x.summary()
